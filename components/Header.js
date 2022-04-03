d3643ae0f8e21beaee6bd7f19aaf0e21754a8fa3
raheemox <draseinabdrahim@gmail.com>
2022-04-03 01:45:37 +0000

    .

--- components/Header.js
+++ components/Header.js
@@ -3,11 +3,13 @@ import Link from 'next/link'
 export default function Header() {
   return (
     <header>
-      <div className='container'>
-        <Link href='/' passHref>
-          <h2>Simple Blog</h2>
-        </Link>
-      </div>
+
+        <ul className='flex'>
+          <Link className='flex-items' href="/" >👌🏻Home</Link>
+          <Link className='flex-items' href="/contact"> 👊🏻Contact </Link>
+          <Link className='flex-items' href="/about">✍🏻About</Link>
+        </ul>
+
     </header>
   )
 }
