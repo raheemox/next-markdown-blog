b0ae75a66759e114fffd5e8904a9ff88580b66a0
raheemox <draseinabdrahim@gmail.com>
2022-04-04 08:23:35 +0000

    m

--- pages/contact.js
+++ pages/contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
             <form className="small-card" name="contact" method="POST" data-netlify="true" action="/success" netlify-honeypot="bot-field">
                 <input className="form" placeholder="enter your email" type="email" name="email" />
                 <textarea placeholder="exapmle: build me a website?" className="form" name="" id="" cols="25" rows="3"></textarea>
-                <button className="btn" type="submit">submit</button>
+                <button className="btn" type="submit">submit this</button>
                 <input type="hidden" name="form-name" value="contact"/>
                 <label className="hidden">
                     Dont fill this out if youre human:
